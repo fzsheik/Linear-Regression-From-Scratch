9bb99fd90c07c4aa63aaa6607eab639c0b51a120
fzsheik <fzsheik14@gmail.com>
2025-03-21 23:44:17 -0400

    just added a method to calculate r squared

--- linRegScratch.py
+++ linRegScratch.py
@@ -9,6 +9,23 @@ data = pd.read_csv('dataset.csv') #just intaking the mock data set
 print(data)
 mp.scatter(data.study_hours, data.test_score)
 mp.show()
+def calculate_r_squared(m, b, data):
+    # Calculate the mean of the test scores
+    y_mean = data.test_score.mean()
+    
+    # Calculate predicted values using your model
+    y_pred = [m * x + b for x in data.study_hours]
+    
+    # Calculate total sum of squares (how much actual values vary from mean)
+    tss = sum((y - y_mean) ** 2 for y in data.test_score)
+    
+    # Calculate residual sum of squares (how much actual values differ from predictions)
+    rss = sum((data.test_score.iloc[i] - y_pred[i]) ** 2 for i in range(len(data)))
+    
+    # Calculate R-squared
+    r_squared = 1 - (rss / tss)
+    
+    return r_squared
 
 def gradient_descent(current_m, current_b, points, learning_rate): 
 #gradient descent is the alogorithm of basically taking each point seperatly and coming up with a m and b, 
@@ -34,13 +51,15 @@ def gradient_descent(current_m, current_b, points, learning_rate):
 
 m = 0 
 b = 0 
-epoch = 100 #number of points we going through
+epoch = 30 #number of points we going through
 learningRate = 0.01
 
 for i in range(epoch):
     m, b = gradient_descent(m, b, data, learningRate)
 
 print(m, b)
+print(calculate_r_squared(m, b, data))
+
 
 mp.scatter(data.study_hours, data.test_score, color="black")
 mp.plot(list(range(0, 15)), [m * x + b for x in range(0, 15)])
